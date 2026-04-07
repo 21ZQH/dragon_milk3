627150c5bf0872dccd3954331639b1f53c3cd88b
Junwei-Lee-0713 <2023213246@bupt.cn>
2026-04-07 16:45:09 +0800

    For uploading resumes

--- webapp/WEB-INF/src/model/TA.java
+++ webapp/WEB-INF/src/model/TA.java
@@ -1,16 +1,66 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TA extends User {
-    private String role="TA";
-    public TA(String password,String email) {
+    private String role = "TA";
+    private List<String> resumeNames = new ArrayList<>();
+    private List<String> resumes = new ArrayList<>();
+    private List<Course> appliedClasses = new ArrayList<>();
+
+    public TA(String password, String email) {
         super(password, email);
     }
+
     @Override
-    public String getRole() { 
-        return role; 
-        
-
-
-        
+    public String getRole() {
+        return role;
     }
+
+    public List<String> getResumeNames() {
+        return resumeNames;
+    }
+
+    //非必要
+    public void setResumeNames(List<String> resumeNames) {
+        this.resumeNames = (resumeNames == null) ? new ArrayList<>() : resumeNames;
+    }
+
+    public List<String> getResumes() {
+        return resumes;
+    }
+
+    public List<Course> getAppliedClasses() {
+        return appliedClasses;
+    }
+
+    public void setAppliedClasses(List<Course> appliedClasses) {
+        this.appliedClasses = (appliedClasses == null) ? new ArrayList<>() : appliedClasses;
+    }
+
+    //非必要
+    public void setResumes(List<String> resumes) {
+        this.resumes = (resumes == null) ? new ArrayList<>() : resumes;
+    }
+
+    //非iteration1
+    public void addResume(String resumeName, String resume) {
+        resumeNames.add(resumeName);
+        resumes.add(resume);
+    }
+
+    public String getResumeByIndex(int index) {
+        if (index < 0 || index >= resumes.size()) {
+            return null;
+        }
+        return resumes.get(index);
+    }
+
+    public void addClass(Course course) {
+        if (course != null) {
+            appliedClasses.add(course);
+        }
+    }
+
 }
